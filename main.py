1122b6dd7e1cebc5d26819fbe3afc9d917dfef22
Dowiw <monjie12272@gmail.com>
2025-05-28 01:03:11 +0200

    skeleton for new library db

--- main.py
+++ main.py
@@ -27,3 +27,27 @@ connection.autocommit = True
 
 
 # Cursor
+cursor = connection.cursor()
+
+# First sql module to create database
+cursor.execute(sql.SQL("CREATE DATABASE {}").format(sql.Identifier(new_db_name)))
+print(f"Database '{new_db_name}' created successfully.")
+
+# Initially connect to new db using root
+new_connection = psycopg2.connect("host = localhost dbname = library user = root password = root")
+cursor = new_connection.cursor()
+
+# Create staff table (id, first_name, last_name, shift)
+cursor.execute("CREATE TABLE IF NOT EXISTS staff("
+"staff_id INT PRIMARY KEY NOT NULL," \
+"staff_first_name TEXT NOT NULL," \
+"staff_last_name TEXT NOT NULL," \
+"staff_shift TEXT NOT NULL")
+
+# Query all records
+cursor.execute("SELECT * FROM staff")
+cursor.execute("SELECT COUNT(*) FROM staff")
+print(cursor.fetchall())
+
+cursor.execute("INSERT INTO staff (staff_id, staff_first_name, staff_last_name, staff_shift)" \
+"VALUES (1, 'John', 'Doe', 'Morning')")
